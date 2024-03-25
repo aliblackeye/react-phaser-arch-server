f6e0f0e2678389029707e1850ad73af12c9edd32
Ali Karagöz <80913896+aliblackeye@users.noreply.github.com>
2024-03-25 05:13:09 +0300

    server update

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -4,28 +4,39 @@ import { Server, Socket } from "socket.io";
 const players = new Map();
 
 const onConnection = (socket: Socket) => {
-  // Oyuncu bağlandığında
-  players.set(socket.id, {
-    playerId: socket.id,
-    x: Math.floor(Math.random() * (1920 * 2)),
-    y: Math.floor(Math.random() * (1080 * 2)),
-  });
-
-  // Diğer oyunculara yeni oyuncuyu ekle
-  socket.broadcast.emit("newPlayer", players.get(socket.id));
-
-  // Yeni oyuncuya mevcut oyuncuları gönder
-  socket.emit("currentPlayers", Array.from(players.values()));
+  // console.log("Player connected: ", socket.id);
 };
 
 const onDisconnect = async (socket: any) => {
-  console.log("Player disconnected: ", socket.id);
+  const player = players.get(socket.id);
+  if (!player) return;
+  console.log("Player left: ", player?.name);
   socket.broadcast.emit("playerLeft", socket.id);
 
   players.delete(socket.id);
 };
 
-const onMovement = (socket: Socket) => {
+const onPlayerJoined = (socket: Socket) => {
+  socket.on("joinGame", async (name) => {
+    console.log("Player joined: ", name);
+    // Oyuncu bağlandığında
+    players.set(socket.id, {
+      name,
+      id: socket.id,
+      x: Math.floor(Math.random() * (1920 * 2)),
+      y: Math.floor(Math.random() * (1080 * 2)),
+      ip: socket.handshake.address,
+    });
+
+    // Diğer oyunculara yeni oyuncuyu ekle
+    socket.broadcast.emit("newPlayer", players.get(socket.id));
+    socket.emit("test", "test");
+    // Yeni oyuncuya mevcut oyuncuları gönder
+    socket.emit("currentPlayers", Array.from(players.values()));
+  });
+};
+
+const onPlayerMovement = (socket: Socket) => {
   // Diğer oyuncuların konumunu güncelle
   socket.on("playerMovement", (movementData) => {
     const player = players.get(socket.id);
@@ -43,7 +54,8 @@ export default (io: Server<any>) => {
   // Bağlantı kurulduğunda
   io.on("connection", async (socket) => {
     onConnection(socket);
-    onMovement(socket);
+    onPlayerJoined(socket);
+    onPlayerMovement(socket);
 
     // Bağlantı kesildiğinde
     socket.on("disconnect", async () => {
